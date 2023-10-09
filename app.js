6ed3619b9591b191a5385a5c1e48c22410035540
Vi-r-us <sahil23012000@gmail.com>
2023-10-09 22:24:57 +0530

    completed the site

--- app.js
+++ app.js
@@ -1,34 +1,91 @@
-//  Credits 
-//  Inspired by Dev Ed https://github.com/developedbyed/3d-card-effect & https://youtu.be/XK7T3mY1V-w 
 
+// variables
+let prev = null;
+const questions = document.querySelectorAll(".question");
+const main = document.querySelector("main");
+const card = document.querySelector(".card");
+const title = document.querySelector(".faq-container h1");
+const platform = document.querySelector(".platform-img img");
+const box = document.querySelector(".box-img img");
+const platformShadow = document.querySelector(".platform-shadow-img img");
 
-jQuery(document).ready(function ($) {
-  //Items
-  var $card = $(".card");
-  var $container = $(".container");
-  var $title = $(".title");
-  var $bike = $(".bike img");
-  var $purchase = $(".purchase");
-  var $description = $(".info h3");
+// FAQ question click event listener
+questions.forEach((question) => {
+  question.querySelector("p").addEventListener("click", e => {
+    // if the same question clicked
+    if (prev === question) {
+      prev.classList.remove("active-question");
+      prev = null;
+      return;
+    }
 
-  //Moving Animation Event
-  $container.on("mousemove", function (e) {
-    let xAxis = (window.innerWidth / 2 - e.clientX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.clientY) / 25;
-    $card.css("transform", `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`);
-  });
+    // disable the previous one
+    if (prev != null) {
+      prev.classList.remove("active-question");
+    }
 
-  //Animate on Hover
-  $container.hover(function () {
-    $card.toggleClass("has-transform");
-    $title.toggleClass("has-transform");
-    $bike.toggleClass("has-transform");
-    $purchase.toggleClass("has-transform");
-    $description.toggleClass("has-transform");
-  });
-
-  //Pop Back on mouseleave
-  $container.on("mouseleave", function () {
-    $card.css("transform", `rotateY(0deg) rotateX(0deg)`);
+    // activate the new one
+    question.classList.add("active-question");
+    prev = question;
   });
 });
+
+
+// 3D animation
+main.addEventListener("mousemove", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+main.addEventListener("touchstart", (e) => {
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+// Animate on Hover
+main.addEventListener("mouseenter", (e) => {
+  // Pop-out elements
+  animateIn()
+});
+main.addEventListener("touchmove", (e) => {
+  // Pop-out elements
+  animateIn()
+});
+
+// Animate Out
+main.addEventListener("mouseleave", (e) => {
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  animateOut();
+});
+main.addEventListener("touchend", (e) => {
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  animateOut();
+});
+
+function animateIn() {
+  platform.classList.add("has-transform");
+  box.classList.add("has-transform");
+  platformShadow.classList.add("has-transform");
+  title.classList.add("has-transform");
+  questions.forEach((question) => {
+    let ques = question.querySelector(".ques");
+    let ans = question.querySelector(".hidden p");
+
+    ques.classList.add("has-transform");
+    ans.classList.add("has-transform");
+  });
+}
+function animateOut() {
+  platform.classList.remove("has-transform");
+  box.classList.remove("has-transform");
+  platformShadow.classList.remove("has-transform");
+  title.classList.remove("has-transform");
+  questions.forEach((question) => {
+    let ques = question.querySelector(".ques");
+    let ans = question.querySelector(".hidden p");
+
+    ques.classList.remove("has-transform");
+    ans.classList.remove("has-transform");
+  });
+}
